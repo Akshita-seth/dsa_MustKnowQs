7b09d73580f1ba291a4d23c71faf8c2a782a7189
Akshita Seth <sethakshita1331@gmail.com>
2026-06-02 12:55:55 +0530

    Add problem link for merge K sorted lists
    Add problem statement link for merging K sorted lists.

---
+++ LinkedList/mergeKSortedLists.cpp
@@ -0,0 +1,10 @@
+// Problem: merge K sorted Lists
+// https://leetcode.com/problems/merge-k-sorted-lists/
+
+
+
+
+
+// To make mergeKLists standard optimized, you need to change the merging strategy:
+//Either use divide & conquer (merge pairs recursively until one list remains).
+//Or use a min-heap to always pick the smallest head efficiently.
