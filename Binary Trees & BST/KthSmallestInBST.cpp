419ccac4363824a630b131b3d0a2bc6066c46be8
Akshita Seth <sethakshita1331@gmail.com>
2026-06-06 00:14:18 +0530

    Update comments in KthSmallestInBST.cpp
    Added reference for BFS approach to Kth smallest element problem.

--- Binary Trees & BST/KthSmallestInBST.cpp
+++ Binary Trees & BST/KthSmallestInBST.cpp
@@ -1,6 +1,7 @@
 // Problem: Kth Smallest Element in BST
 // https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 
+// FOR BFS: https://takeuforward.org/data-structure/kth-largest-smallest-element-in-binary-search-tree
 
 // Optimized: Using global variable and DFS Inorder Traversal
 //TC: O(N), where N is the number of nodes in the binary tree. The reason is that in the worst-case scenario, the inorder and reverse inorder traversals visit each node exactly once.
