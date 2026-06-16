d13926d110d3c67b3d4ad10ef31a058c564bc67e
Akshita Seth <sethakshita1331@gmail.com>
2026-06-16 23:46:52 +0530

    Add solution for Kth Largest Element in Array
    Implement Kth largest element finder using sorting.

--- Heaps/KthLargestInArray.cpp
+++ Heaps/KthLargestInArray.cpp
@@ -1,2 +1,18 @@
 // Problem: Kth Largest Element In Array
 // https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+
+// With sorting:
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int i=0;
+        while(i < nums.size()-k)
+        {
+            i++;
+        }
+        return nums[i];
+    }
+};
