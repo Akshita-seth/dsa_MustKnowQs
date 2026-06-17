c4c718cdf8f0711ca3a6b4703b8e2ef47e7d6d68
Akshita Seth <sethakshita1331@gmail.com>
2026-06-17 18:19:41 +0530

    Optimize Kth largest element search using min-heap
    Implemented an optimized solution to find the Kth largest element using a min-heap. This approach improves time complexity to O(n log k) and space complexity to O(k).

--- Heaps/KthLargestInArray.cpp
+++ Heaps/KthLargestInArray.cpp
@@ -11,3 +11,33 @@ public:
         return nums[nums.size()-k]; 
     }
 };
+
+
+// Optimized using heaps: complimentary of Kth Samllest
+// S1-> create a min Heap with the first K elements
+// S2- for the rest of the elements check => if element > heap.top() => heap.pop() => heap.push(element)
+// This will result in the heap containing the first K largest elements, and the root of this heap will be the Kth largest element (this is why we took min-heap).
+
+// TC: O(nlogk) => building a heap O(k), Processing the remaining (n-k) elements each push/pop is O(logk)
+// SC: O(k)
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for(int i=0; i<k; i++)
+          minHeap.push(nums[i]);
+        
+        for(int i=k; i<nums.size(); i++)
+        {
+            if(nums[i] > minHeap.top())
+            {
+                minHeap.pop();
+                minHeap.push(nums[i]);
+            }
+        }
+        return minHeap.top();
+    }
+};
