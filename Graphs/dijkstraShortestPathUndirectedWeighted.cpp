b6219915709ba3001e64372a4c9a5d13ec524c79
Akshita Seth <sethakshita1331@gmail.com>
2026-06-26 12:54:18 +0530

    Refactor Dijkstra's algorithm for undirected graphs
    Updated Dijkstra's algorithm implementation to handle undirected weighted graphs using a set for distance tracking and adjacency list for graph representation.

--- Graphs/dijkstraShortestPathUndirectedWeighted.cpp
+++ Graphs/dijkstraShortestPathUndirectedWeighted.cpp
@@ -8,8 +8,8 @@
 // Since in Dijkstra we push (distance, node), the smallest distance automatically comes to the top.
 // Doesn't work fro neative weights
 
-// TC:
-
+// TC: O(ElogV) as each edge leads to at most one insertion in the priority queue, which takes log V time.
+// SC: O(V + E), due to the distance array and adjacency list storing all vertices and edges.
 
 class Solution {
   public:
@@ -51,6 +51,64 @@ class Solution {
                   {
                       distVec[v] = distU + wt;
                       minHeap.push({distVec[v],v}); // {dist,node} pair in heap
+                  }
+            }
+        }
+        return distVec;
+    }
+};
+
+
+// USing Set => st.erase() can be done 
+
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        // Build Adj list
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto e: edges)
+        {
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt}); // since undirected
+        }
+        // Set used {dist,node}
+        set<pair<int,int>> st;
+        vector<int> distVec(V,1e9);
+        
+        distVec[src] = 0;
+        st.insert({0,src});
+        
+        // Dijkstra's Loop
+        while(!st.empty())
+        {
+            // no top access here, iterators work here
+            auto it = *(st.begin()); // st.begin() gives iterator thus * => value at the iterator
+            
+            int u = it.second;
+            int distU = it.first;
+            st.erase(it); // no pop here, its erase
+            
+            for(auto adjNode: adj[u])
+            {
+                int v = adjNode.first;
+                int wt = adjNode.second;
+                
+                // no need to visit comfirmed long paths for that node
+                if (distU > distVec[u]) continue; // without this not all test acses were getting passed
+                
+                if(distVec[v] > distU + wt)
+                  {
+                      //if already some path exist then remove it as it isnlonger than the new encountered one
+                      if(distVec[v] != 1e9) 
+                      {
+                          st.erase({distVec[v],v});
+                      }
+                      distVec[v] = distU + wt;
+                      st.insert({distVec[v],v}); // {dist,node} pair in set
                   }
             }
         }
