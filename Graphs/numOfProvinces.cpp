f73103e79333c12554208e74a0a0f7c0b4dbfd07
Akshita Seth <sethakshita1331@gmail.com>
2026-06-29 12:55:08 +0530

    Create numOfProvinces.cpp for province counting
    Added a new file for counting the number of provinces in a graph.

---
+++ Graphs/numOfProvinces.cpp
@@ -0,0 +1,5 @@
+// Problem: Count Number of Provinces / Strongly Connected Components in a graph
+// https://www.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1
+// https://leetcode.com/problems/number-of-provinces/description/
+
+
