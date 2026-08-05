97e453fc315f5b17643c880a7c656ea9748f5b14
Akshita Seth <sethakshita1331@gmail.com>
2026-08-05 11:38:32 +0530

    Fix profit calculation in buy and sell stocks

--- Array/buy&sellStocks.cpp
+++ Array/buy&sellStocks.cpp
@@ -16,7 +16,7 @@ int maxProfit(vector<int> &prices) {
     int profit = 0;
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
-			profit = max(res, prices[j] - prices[i]);
+			profit = max(profit, prices[j] - prices[i]);
         }
     }
     return profit;
