a60bb51a33043a8853130e47327578a21b906e57
Akshita Seth <sethakshita1331@gmail.com>
2026-08-10 00:11:45 +0530

    Implement iterative LCA in BST
    Added iterative approach for finding LCA in BST.

--- Binary Trees & BST/LCAInBST.cpp
+++ Binary Trees & BST/LCAInBST.cpp
@@ -1,9 +1,43 @@
 // Problem: LCA in BST
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
-//       Generic Binary Tree LCA	                BST-Based LCA
-// TC:      𝑂(𝑁)                                       O(H)
-// SC:     𝑂(H)                                        O(1)
+//       Generic Binary Tree LCA	 BST based Recursive                BST-BasedIterative
+// TC:      𝑂(𝑁)                       O(H)                                     O(H)
+// SC:     𝑂(H)                         O(H)                                    O(1)
+
+// Better than generic BT soln:
+// Using BST properties recursively
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL || root == p || root == q)
+        return root;
+
+        if(p->val < root->val && q->val < root->val)
+        return lowestCommonAncestor(root->left, p, q);
+
+        else if(p->val > root->val && q->val > root->val)
+        return lowestCommonAncestor(root->right, p, q);
+        
+        else 
+        return root;
+    }
+};
+
+
+// Optimized: Using BST Properties Iteratively
 
 class Solution {
 public:
