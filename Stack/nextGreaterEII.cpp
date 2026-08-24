e9c87a10853315560e8aee5bbac02302a7d00c74
Akshita Seth <sethakshita1331@gmail.com>
2026-08-24 12:41:05 +0530

    Update nextGreaterEII.cpp

--- Stack/nextGreaterEII.cpp
+++ Stack/nextGreaterEII.cpp
@@ -41,4 +41,29 @@ for(int i=0; i<n; i++)
         }
 
 
-// OS:
+// OS: Monotonic stack, Hypothetically doibled the array
+// TC: O(2N), SC: O(2N) for stack (worst) + O(N) for ans
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> nge(n,-1);
+        stack<int> st;
+
+        // Double the array hypothetically
+        for(int i=2*n-1; i>=0; i--)
+        {
+            int idx = i%n;
+            while(!st.empty() && st.top() <= nums[idx])
+            st.pop();
+            if(i < n)
+            {
+                nge[i] = st.empty()?-1 : st.top();
+            }
+            st.push(nums[idx]);
+        }
+        return nge;
+    }
+};
