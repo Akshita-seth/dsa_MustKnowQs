8f70132625533cb134f6d051d625b404af6b823c
Akshita Seth <sethakshita1331@gmail.com>
2026-08-27 20:19:25 +0530

    Update KthLargestInArray.cpp

--- Heaps/KthLargestInArray.cpp
+++ Heaps/KthLargestInArray.cpp
@@ -41,3 +41,81 @@ public:
         return minHeap.top();
     }
 };
+
+
+// OS: TC: O(N) SC: O(1)
+// O(N), where N is the size of the given array.
+// In the average case (when the pivot is chosen randomly):
+// Assuming the array gets divided into two equal parts, with every partitioning step, the search range is reduced by half. Thus, the time complexity is O(N + N/2 + N/4 + ... + 1) = O(N).
+
+// In the worst-case scenario (when the element at the left or right index is chosen as the pivot):
+// In such cases, the array is divided into two unequal halves, and the search range is reduced by one element with every partitioning step. Thus, the time complexity is O(N + N-1 + N-2 + ... + 1) = O(N2). 
+// However, the probability of this worst-case scenario is negligible.
+
+
+
+class Solution {
+private:
+    // Function to get a random index 
+    int randomIndex(int &left, int &right) {
+        // length of the array 
+        int len = right - left + 1;
+        
+        // Return a random index from the array 
+        return (rand() % len) + left;
+    }
+    
+    // Function to perform the partition and return the updated index of pivot
+    int partitionAndReturnIndex(vector<int> &nums, int pivotIndex, int left, int right) {
+        int pivot = nums[pivotIndex]; // Get the pivot element
+        
+        // Swap the pivot with the left element
+        swap(nums[left], nums[pivotIndex]);
+        
+        int ind = left + 1; // Index to mark the start of right portion
+        
+        // Traverse on the array 
+        for(int i = left + 1; i <= right; i++) {
+            
+            // If the current element is greater than the pivot
+            if(nums[i] > pivot) {
+                // Place the current element in the left portion
+                swap(nums[ind], nums[i]);
+                
+                // Move the right portion index
+                ind++;
+            }
+        }
+        
+        swap(nums[left], nums[ind-1]); // Place the pivot at the correct index
+        
+        return ind-1; // Return the index of pivot now
+    }
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        // Return -1, if the Kth largest element does not exist
+        if(k > nums.size()) return -1;
+        
+        // Pointers to mark the part of working array 
+        int left = 0, right = nums.size() - 1;
+        
+        // Until the Kth largest element is found
+        while(true) {
+            // Get the pivot index
+            int pivotIndex = randomIndex(left, right);
+            
+            // Update the pivotIndex
+            pivotIndex = partitionAndReturnIndex(nums, pivotIndex, left, right);
+            
+            // If Kth largest element is found, return
+            if(pivotIndex == k-1) return nums[pivotIndex];
+            
+            // Else adjust the end pointers in array 
+            else if(pivotIndex > k-1) right = pivotIndex - 1;
+            else left = pivotIndex + 1;
+        }
+        
+        return -1;
+    }
+};
