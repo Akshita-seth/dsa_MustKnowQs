c3491805af7a758fbe4e7a6b06cff4204b2ceba8
Akshita Seth <sethakshita1331@gmail.com>
2026-09-07 23:59:59 +0530

    Update numOfProvinces.cpp

--- Graphs/numOfProvinces.cpp
+++ Graphs/numOfProvinces.cpp
@@ -7,6 +7,7 @@
 // Union-Find (Disjoint Set Union): merge connected nodes, count distinct parents.
 
 
+//Component-wise calculation using DFS/BFS
 // TC: O(V+E), we visit every node and for every node we visit all of its neighbours in the DFS traversal.
 // SC: O(V), for storing visited array and auxiliary stack space.
 
@@ -59,5 +60,99 @@ public:
             }
         }
         return count;
+    }
+};
+
+
+
+//Disjoint set approach:
+// TC: O(N^2) [i, j loop in findCircNum fn] + O(alpha) which is constant 
+// SC: O(n) -> rank or size and parent
+
+class DisjointSet{
+    public:
+    vector<int> rank, size, parent;
+    DisjointSet(int n)
+    {
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+
+        for(int i=0; i<n; i++)
+        parent[i] = i;
+    }
+    
+    int findUPar(int node)
+    {
+        if(parent[node] == node)
+        return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_v = findUPar(v);
+        int ulp_u = findUPar(u);
+
+        if(ulp_u == ulp_v)
+        return;
+
+        if(rank[ulp_u] < rank[ulp_v])
+        parent[ulp_u] = ulp_v; 
+        else if(rank[ulp_v] < rank[ulp_u])
+        parent[ulp_v] = ulp_u;
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        } 
+    }
+    void unionBySize(int u, int v)
+    {
+        int ulp_v = findUPar(v);
+        int ulp_u = findUPar(u);
+
+        if(ulp_u == ulp_v)
+        return;
+
+        if(size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else 
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        int provinces = 0;
+
+        DisjointSet ds(n);
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(isConnected[i][j] == 1)
+                {
+                    ds.unionBySize(i,j);
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(ds.findUPar(i) == i)
+            provinces++;
+        }
+
+        return provinces;
     }
 };
